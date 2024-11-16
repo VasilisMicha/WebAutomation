f06dc3d6eed829bc0d61c1262ad62eb860a82000
VasilisMicha <vasilismich789@gmail.com>
2024-11-16 10:56:57 +0200

    changed login credentials

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import com.google.gson.JsonObject;
 
 public class Main {
     public static void main(String[] args) {
-        String email = "dionusiavruna11@gmail.com";
+        String email = "testing@gmail.com";
         String password = "1";
-        String server = "gr22";
+        String server = "gr1";
         LogIn login = new LogIn();
         login.logingIn(email, password, server);
         JsonObject dataJson = login.getDataJson();
